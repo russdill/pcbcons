3d1aebafe6b4f850563c0e766957f025d50ea7f6
Robert Spanton <rspanton@zepler.net>
2011-08-07 23:19:49 +0100

    Adjust test utility to use array of pads

--- test.py
+++ test.py
@@ -3,23 +3,12 @@ from pcons import Point, FixedDist, X, Y, Pad, Align
 from pcons import Val as V
 from decimal import Decimal as D
 import pcons
+import render_pcb
 
-p = Pad( (D(10), D(20)) )
-q = Pad( (D(10), D(20)) )
+pads, cons = pcons.pad_array( (D(10), D(20)), 10, X, D(12) )
 
-cons = []
-cons += pcons.set_origin( p.corners[Pad.BL] )
+cons += pcons.set_origin( pads[0].corners[Pad.BL] )
 
-cons.append( FixedDist( D(50),
-                        p.corners[Pad.BL].x,
-                        q.corners[Pad.BL].x ) )
-
-cons += Align( [ p.corners[Pad.BL],
-                 q.corners[Pad.BL] ], Y )
-
-pcons.resolve( [p,q], cons )
-
-print p
-print q
-
+pcons.resolve( pads, cons )
+render_pcb.render( pads )
 
